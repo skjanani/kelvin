32b50367e62a7c80aedccf70e7eb6da66fd50d47
skjanani <41111202+skjanani@users.noreply.github.com>
2018-07-28 11:48:23 +0530

    Add files via upload

---
+++ Kelvin.java
@@ -0,0 +1,15 @@
+package janani;
+import java.util.Scanner;
+public class Kelvin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n,n1;
+System.out.println("enter the temperature in celsius");
+Scanner ja=new Scanner(System.in);
+n1=ja.nextInt();
+n=n1+273;
+System.out.println(n);
+	}
+
+}
